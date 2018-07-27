d3d74044ed6a4913916340569c0f54ce9398a83f
ChaiYe <2920118417@qq.com>
2018-07-27 20:18:51 +0800

    订单逻辑(Chrome上运行显示不了Address页面)

--- src/view/Address.vue
+++ src/view/Address.vue
@@ -2,37 +2,12 @@
 
     <div>
       <XmHead></XmHead>
-      <XmCrunmbs></XmCrunmbs>
+      <XmCrunmbs pageName="地址" curPath="/Address">
+      </XmCrunmbs>
 
       <div class="ad_main">
         <div class="ad_main_content">
-          <Orderheader></Orderheader>
-          <!--<ul class="process">
-            <li>
-              <span class="ad_pro_title_active">确认地址</span>
-              <div class="ad_line ad_line_active">
-                <div class="ad_cirle ad_cirle_active"></div>
-              </div>
-            </li>
-            <li>
-              <span>查看订单</span>
-              <div class="ad_line">
-                <div class="ad_cirle"></div>
-              </div>
-            </li>
-            <li>
-              <span>支付</span>
-              <div class="ad_line">
-                <div class="ad_cirle "></div>
-              </div>
-            </li>
-            <li>
-              <span>订单确认</span>
-              <div class="ad_line">
-                <div class="ad_cirle"></div>
-              </div>
-            </li>
-          </ul>-->
+          <Orderheader  ></Orderheader>
 
           <div class="ad_content">
             <h3 class="ad_title">收货地址</h3>
@@ -43,8 +18,8 @@
                 <p class="ad_tel">{{item.Tel}}</p>
                 <div>
                   <h5 class="ad_default" v-show="item.isDefault">默认地址</h5>
-                  <h5 class="ad_default set_default" v-show="!item.isDefault" @click="setDefalt(item.Id)">设为默认地址</h5>
-                  <span @click="itemId=item.Id;delConfirm=true">&#xe613;</span>
+                  <h5 class="ad_default set_default" v-show="!item.isDefault" @click="setAddrDefault(item.Id)">设为默认地址</h5>
+                  <span @click="delAddr(item.Id)">删除</span>
                 </div>
               </li>
               <li class="ad_addAdr">
@@ -53,13 +28,13 @@
               </li>
             </ul>
           </div>
-          <div class="ad_more" @click="expand " :class="{'ad_more_expand':limit>3}">more<span>&#xe60c;</span></div>
+          <div class="ad_more" @click="expand" :class="{'ad_more_expand':limit>3}">more</div>
           <div class="ad_next">
             <router-link class="ad_next_a" :to="{path:'orderConfirm',query:{addressId:selectedId}}">下一步</router-link>
           </div>
         </div>
       </div>
-     <!-- <modal v-show="delConfirm" @close="delConfirm=false">
+     <!--<modal v-show="delConfirm" @close="delConfirm=false">
         <div slot="message" class="confirm_tip" >
           <p style="margin-left: 123px">您确认删除此地址吗？</p>
         </div>
@@ -75,11 +50,61 @@
 </template>
 
 <script>
+
   import XmCrunmbs from '../components/common/XmCrumbs.vue'
   import XmHead from '../components/common/XmHead.vue'
   import XmFoot from '../components/common/XmFoot.vue'
   import  Orderheader from '../components/common/Orderheader'
+  import axios from 'axios'
   export default {
+    data(){
+      return {
+        addrList:[
+          {
+          Id:1,
+          Name:'name',
+          Address:'Address',
+          Tel:'123456',
+            isDefault:true
+          },
+          {
+            Id:2,
+            Name:'name',
+            Address:'Address',
+            Tel:'123456',
+            isDefault:false
+          },
+          {
+            Id:3,
+            Name:'name',
+            Address:'Address',
+            Tel:'123456',
+            isDefault:false
+          },
+          {
+            Id:4,
+            Name:'name',
+            Address:'Address',
+            Tel:'123456',
+            isDefault:false
+          },
+          {
+            Id:1,
+            Name:'name',
+            Address:'Address',
+            Tel:'123456',
+            isDefault:false
+          }
+        ],
+        limit:3,
+        activeIndex:0,
+        delConfirm:false,
+        itemId:'',
+        selectedId:'0',
+
+      }
+    },
+
     components: {
       XmCrunmbs,
       XmHead,
@@ -87,10 +112,66 @@
       XmFoot
 
     },
+    mounted(){
+      this.getAddressList();
+    },
+    computed:{
+      /*控制地址显示数量*/
+      addrListFilter(){
+        return this.addrList.slice(0,this.limit);
+      }
+    },
+
+    methods:{
+      /*获得用户地址列表*/
+      getAddressList(){
+        axios.get('api/users/addressList').then((response)=>{
+          console.log(response);
+          var res =response.data;
+          if(res.status=='0'){
+            this.addrList=res.result;
+          }
+          else{
+            console.log(res);
+          }
+        })
+      },
+      /*设置默认地址*/
+      setAddrDefault(id){
+        axios.post('api/users/setDefault',{
+          Id:id
+        }).then((response)=>{
+          var res =response.data;
+          if(res.status=='0'){
+            this.getAddressList()
+          }
+        })
+      },
+      /*控制地址显示的数量*/
+      expand(){
+        if(this.limit==3){
+          this.limit=this.addrList.length;
+        }else {
+          this.limit=3
+        }
+      },
+      /*删除默认地址*/
+      delAddr(id){
+        axios.post('api/users/addrDel',{
+          Id:id
+        }).then((response)=>{
+          var res= response.data;
+          if(res.status=='0'){
+            this.delConfirm=false;
+            this.getAddressList();
+          }
+         })
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style lang="less">
   h1,h2,h3,h4,h5,h6{
     margin: 0;
   }
@@ -98,139 +179,110 @@
     width: 100%;
     height: auto;
     background-color: #F5F7FC;
-  }
-  .ad_main_content{
-    width: 1220px;
-    height: auto;
-    margin: 0 auto;
-    padding: 0 20px;
-  }
-  .process{
-    display: flex;
-    width: 100%;
-  }
-  .process li{
-    width: 305px;
-    padding: 40px 0;
-    text-align: center;
-    color: #CAC8CE;
-  }
-  .ad_line{
-    position: relative;
-    width: 305px;
-    height: 2px;
-    margin-top: 20px;
-    background-color: #CAC8CE;
-  }
-  .ad_cirle{
-    position: absolute;
-    left: 147px;
-    top: -6px;
-    width: 12px;
-    height: 12px;
-    border-radius: 6px;
-    background-color: #CAC8CE;
-  }
-  .ad_line_active,.ad_cirle_active{
-    background-color: #d1434a;
-  }
-  .process .ad_pro_title_active{
-    color: #d1434a;
-  }
-  .process_active li{
-    color: #d1434a;
-  }
-  .ad_content{
-    width: 1260px;
-    height: auto;
-  }
-  .ad_title{
+    .ad_main_content{
+      width: 1220px;
+      height: auto;
+      margin: 0 auto;
+      padding: 0 20px;
+    }
+    .process{
+      display: flex;
+      width: 100%;
+    }
 
-  }
-  .ad_items{
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    height: auto;
-    margin-top: 20px;
-  }
-  .ad_items li{
-    width: 290px;
-    height: 180px;
-    padding: 20px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    box-sizing: border-box;
-    background-color: white;
-    border: 2px solid #EFEDF4;
-    cursor: pointer;
-  }
-  .ad_items .li_active,.ad_items li:hover{
-    border: 2px solid #ee7a23;
-  }
-  .ad_items div{
+    .ad_content{
+      width: 1260px;
+      height: auto;
+    }
+    .ad_title{
 
+    }
+    .ad_items{
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
+      height: auto;
+      margin-top: 20px;
+    }
+    .ad_items li{
+      width: 290px;
+      height: 180px;
+      padding: 20px;
+      margin-right: 20px;
+      margin-bottom: 20px;
+      box-sizing: border-box;
+      background-color: white;
+      border: 2px solid #EFEDF4;
+      cursor: pointer;
+    }
+    .ad_items .li_active,.ad_items li:hover{
+      border: 2px solid #ee7a23;
+    }
+    .ad_items div{
+
+    }
+    .ad_items div span{
+      font-family: 'iconfont';
+      cursor: pointer;
+      float: right;
+    }
+    .ad_address,.ad_tel,.ad_items div{
+      margin-top: 15px;
+    }
+    .ad_items div span:hover,.ad_default{
+      color: #ee7a23;
+    }
+    .ad_default{
+      float: left;
+    }
+    .set_default{
+      display: none;
+    }
+    .ad_items li:hover .set_default{
+      display: block;
+    }
+    .set_default:hover{
+      color: #d1434a;
+    }
+    .ad_items .ad_addAdr{
+      text-align: center;
+    }
+    .ad_items .ad_addAdr span{
+      font-size: 60px;
+      cursor: pointer;
+    }
+    .ad_items .ad_addAdr span:hover{
+      color: #ee7a23;
+    }
+    .ad_more{
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      height: 20px;
+      line-height: 20px;
+      color: #ee7a23;
+      cursor: pointer;
+    }
+    .ad_more span{
+      font-family: iconfont;
+      display: block;
+      transition: all .5s;
+    }
+    .ad_more_expand span{
+      transform: rotate(180deg);
+    }
+    .ad_next{
+      float: right;
+      width: 100px;
+      height: 40px;
+      background-color: #d1434a;
+      text-align: center;
+      line-height: 40px;
+      cursor: pointer;
+    }
+    .ad_next_a{
+      color: white;
+    }
   }
-  .ad_items div span{
-    font-family: 'iconfont';
-    cursor: pointer;
-    float: right;
-  }
-  .ad_address,.ad_tel,.ad_items div{
-    margin-top: 15px;
-  }
-  .ad_items div span:hover,.ad_default{
-    color: #ee7a23;
-  }
-  .ad_default{
-    float: left;
-  }
-  .set_default{
-    display: none;
-  }
-  .ad_items li:hover .set_default{
-    display: block;
-  }
-  .set_default:hover{
-    color: #d1434a;
-  }
-  .ad_items .ad_addAdr{
-    text-align: center;
-  }
-  .ad_items .ad_addAdr span{
-    font-size: 60px;
-    cursor: pointer;
-  }
-  .ad_items .ad_addAdr span:hover{
-    color: #ee7a23;
-  }
-  .ad_more{
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    height: 20px;
-    line-height: 20px;
-    color: #ee7a23;
-    cursor: pointer;
-  }
-  .ad_more span{
-    font-family: iconfont;
-    display: block;
-    transition: all .5s;
-  }
-  .ad_more_expand span{
-    transform: rotate(180deg);
-  }
-  .ad_next{
-    float: right;
-    width: 100px;
-    height: 40px;
-    background-color: #d1434a;
-    text-align: center;
-    line-height: 40px;
-    cursor: pointer;
-  }
-  .ad_next_a{
-    color: white;
-  }
+
 </style>
--- src/view/OrderConfirm.vue
+++ src/view/OrderConfirm.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <XmHead></XmHead>
-    <XmCrunmbs>
-      <a href="javascript:;">确认订单</a>
+    <XmCrunmbs pageName="订单确认" curPath="/OrderConfirm">
     </XmCrunmbs>
 
     <div class="main">
 
       <div class="main_content">
-        <Orderheader></Orderheader>
+        <Orderheader class="Orderheader"></Orderheader>
         <h3 class="alltitle">订单详情</h3>
         <table class="tb" cellspacing="0">
           <tr class="tb_header">
@@ -17,116 +16,49 @@
             <th>数量</th>
             <th>总价</th>
           </tr>
-          <tr>
-          <td class="tbfirst">
-            <img src="http://hotemotion.fun:3389/static/16.jpg" alt="">
-          </td>
-          <td class="title">
-            x小米小爱音箱
-          </td>
-          <td>
-            <span>￥149.00</span>
-          </td>
-          <td>
-            <span>*5</span>
-          </td>
-          <td>
-            <span>￥745.0</span>
-          </td>
-        </tr>
-          <tr>
+          <tr v-for="item of orderList" v-if="item.checked==1">
             <td class="tbfirst">
-              <img src="http://hotemotion.fun:3389/static/16.jpg" alt="">
+              <img :src="'../../static/'+item.productImage" alt="">
             </td>
             <td class="title">
-              x小米小爱音箱
+              {{item.productName}}
             </td>
             <td>
-              <span>￥149.00</span>
+              <span>￥{{item.salePrice }}</span>
             </td>
             <td>
-              <span>*5</span>
+              <span>*{{item.productNum}}</span>
             </td>
             <td>
-              <span>￥745.0</span>
+              <span>￥{{(item.productNum*item.salePrice)}}</span>
             </td>
           </tr>
-          <tr>
-            <td class="tbfirst">
-              <img src="http://hotemotion.fun:3389/static/16.jpg" alt="">
-            </td>
-            <td class="title">
-              x小米小爱音箱
-            </td>
-            <td>
-              <span>￥149.00</span>
-            </td>
-            <td>
-              <span>*5</span>
-            </td>
-            <td>
-              <span>￥745.0</span>
-            </td>
-          </tr>
-          <tr>
-            <td class="tbfirst">
-              <img src="http://hotemotion.fun:3389/static/16.jpg" alt="">
-            </td>
-            <td class="title">
-              x小米小爱音箱
-            </td>
-            <td>
-              <span>￥149.00</span>
-            </td>
-            <td>
-              <span>*5</span>
-            </td>
-            <td>
-              <span>￥745.0</span>
-            </td>
-          </tr>
-          <tr>
-            <td class="tbfirst">
-              <img src="http://hotemotion.fun:3389/static/16.jpg" alt="">
-            </td>
-            <td class="title">
-              x小米小爱音箱
-            </td>
-            <td>
-              <span>￥149.00</span>
-            </td>
-            <td>
-              <span>*5</span>
-            </td>
-            <td>
-              <span>￥745.0</span>
-            </td>
-          </tr>
+
         </table>
         <div class="result">
           <ul>
             <li>
               商品总价:
-              <span>￥4,679.00</span>
+              <span>￥{{subTotal}}</span>
             </li>
             <li>
               快递费:
-              <span>￥10.00</span>
+              <span>￥{{courier_fees}}</span>
             </li>
             <li>
               折扣:
-              <span>￥200.00</span>
+              <span>￥{{discount}}</span>
             </li>
             <li>
               合计:
-              <span id="total_price">￥4,489.00</span>
+              <span id="total_price">￥{{(subTotal+courier_fees-discount)}}</span>
             </li>
           </ul>
         </div>
         <div class="clear"></div>
         <div class="btn_content">
-          <route-link to="/" class="btn previous">上一步</route-link>
-          <route-link to="/" class="btn confirm">确认支付</route-link>
+          <router-link to="{path:'Address'}" class="btn previous">上一步</router-link>
+          <div @click="payMent" class="btn confirm">确认支付</div>
         </div>
       </div>
     </div>
@@ -139,118 +71,185 @@
   import XmHead from '../components/common/XmHead.vue'
   import XmFoot from '../components/common/XmFoot.vue'
   import  Orderheader from '../components/common/Orderheader'
+  import axios from 'axios'
     export default {
+      data(){
+        return{
+          orderList:[
+            {
+              productName:'文具',
+              salePrice:12,
+              productNum:2,
+              checked:1
+            },
+            {
+              productName:'文具',
+              salePrice:12,
+              productNum:2,
+              checked:1
+            }
+          ],
+          courier_fees:0,
+          discount:0,
+
+        }
+      },
       components: {
         XmCrunmbs,
         XmHead,
         XmFoot,
         Orderheader
       },
+      mounted(){
+        this.init();
+      },
+      computed:{
+        subTotal(){
+          var sub=0;
+          this.orderList.forEach((item)=>{
+            if(item.checked=='1'){
+              sub+=item.productNum*item.salePrice
+            }
+          })
+
+          return sub;
+        }
+      },
+      methods:{
+        init(){
+          axios.get('api/users/carList').then((response)=>{
+            var res=response.data;
+            this.carList=res.result;
+          })
+        },
+        payMent(){
+          var addressId=this.$route.query.addressId;
+          axios.post('api/users/payMent',{
+            productList:this.orderList,
+            addressId:addressId,
+            orderTotal:this.total_price+this.courier_fees-this.discount,
+            shipping:this.courier_fees,
+            discount:this.discount
+          }).then((response)=>{
+            var res= response.data;
+            if(res.status=='0'){
+              this.$router.push({
+                path:'/orederSuccess?orderId='+res.result
+              })
+            }
+          })
+        }
+      }
     }
 </script>
 
-<style scoped>
+<style lang="less">
   .main{
-   padding: 24px;
+    padding: 24px;
     background: #F5F7FC;
-  }
-  li{
-    list-style: none;
-  }
-  .main_content{
-    width: 80%;
-    margin: 0 auto;
-  }
-  .alltitle{
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 24px;
+    li{
+      list-style: none;
+    }
+    .main_content{
+      width: 80%;
+      margin: 0 auto;
+    }
+    .alltitle{
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 24px;
+    }
+    .Orderheader{
+      width: 100%;
+      margin: 0 auto;
+    }
+
+    /*单个物品*/
+    tr{
+      height: 80px;
+      padding:10px 0;
+    }
+    .tb tr img{
+      width: 48px;
+      height: 48px;
+      border: 1px solid rgba(0,0,0,.1);
+      margin: 0 24px;
+    }
+    .item_name{
+      vertical-align: middle;
+      text-align: center;
+      line-height: 48px;
+    }
+    /*表格*/
+    .tb{
+      width: 100%;
+      margin: 0 auto;
+      border: 1px solid rgba(0,0,0,.1);
+
+    }
+    .tb_header{
+      width: 100%;
+      height: auto ;
+      margin: 0 auto;
+    }
+    .tb_header th{
+      background: #605f61;
+      color: white;
+      line-height: 40px;
+    }
+    /*订单信息汇总*/
+    .result{
+      float: right;
+      width: 240px;
+      font-size: 14px;
+    }
+    .result ul li{
+      text-align: left;
+    }
+    .result ul span{
+      float: right;
+      width:80px;
+      color: black;
+    }
+    .result ul>li{
+      margin: 12px;
+      color: #c8c6cd;
+    }
+    #total_price{
+      color: #d1434a;
+    }
+    /*清除浮动*/
+    .clear {
+      clear:both;
+      height: 0;
+      line-height: 0;
+      font-size: 0;
+    }
+    /*按钮*/
+    .btn_content{
+      margin: 12px 0;
+    }
+    .btn{
+      text-align: center;
+      background: brown;
+      width: 100px;
+      height: 40px;
+      color:white;
+      line-height: 40px;
+    }
+    .previous{
+      float: left;
+    }
+    .confirm{
+      float: right;
+    }
+    .tbfirst{
+      width: 5%;
+    }
+    .title{
+      text-align: left;
+    }
   }
 
-  /*单个物品*/
-  tr{
-    height: 80px;
-    padding:10px 0;
-  }
-  .tb tr img{
-    width: 48px;
-    height: 48px;
-    border: 1px solid rgba(0,0,0,.1);
-    margin: 0 24px;
-  }
-  .item_name{
-    vertical-align: middle;
-    text-align: center;
-    line-height: 48px;
-  }
-  /*表格*/
-  .tb{
-    width: 100%;
-    margin: 0 auto;
-    border: 1px solid rgba(0,0,0,.1);
-
-  }
-  .tb_header{
-    width: 100%;
-    height: auto ;
-    margin: 0 auto;
-  }
-  .tb_header th{
-    background: #605f61;
-    color: white;
-    line-height: 40px;
-  }
-  /*订单信息汇总*/
-  .result{
-    float: right;
-    width: 240px;
-    font-size: 14px;
-  }
-  .result ul li{
-    text-align: left;
-  }
-  .result ul span{
-    float: right;
-    width:80px;
-    color: black;
-  }
-  .result ul>li{
-    margin: 12px;
-   color: #c8c6cd;
-  }
-  #total_price{
-    color: #d1434a;
-  }
-  /*清除浮动*/
-  .clear {
-    clear:both;
-    height: 0;
-    line-height: 0;
-    font-size: 0;
-  }
-  /*按钮*/
-  .btn_content{
-    margin: 12px 0;
-  }
-  .btn{
-    text-align: center;
-    background: brown;
-    width: 100px;
-    height: 40px;
-    color:white;
-    line-height: 40px;
-  }
-  .previous{
-    float: left;
-  }
-  .confirm{
-    float: right;
-  }
-  .tbfirst{
-   width: 5%;
- }
-  .title{
-   text-align: left;
-  }
 
 </style>
